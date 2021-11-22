dfccdfd804b998ddc03cd72aa1161829a8afef76
Jaron-Lane <jaronwohlwend@icloud.com>
2021-11-22 13:03:00 -0600

    created customer count and employee specialties display

--- src/components/customers/CustomerList.js
+++ src/components/customers/CustomerList.js
@@ -1,24 +1,40 @@
 import React, { useEffect, useState } from 'react';
 
 export const CustomerList = () => {
-    const [ customers, setCustomers ] = useState([])
+    const [ customers, setCustomers ] = useState([]);
+    const [ customerMessage, setMessage ] = useState("");
 
     useEffect(
         () => {
             fetch("http://localhost:8088/customers")
                 .then(res => res.json())
-                .then(setCustomers)
+                .then((data) => {
+                    setCustomers(data)
+                })
         },
         []
+    )
+
+    useEffect(
+        () => {
+            if (customers.length === 1) {
+                setMessage("You have 1 customer")
+            } 
+            else {
+                setMessage(`You have ${customers.length} customers`)
+            }
+        },
+        [customers]
     )
 
     return (
         <>
             <h2>Customers List</h2>
+            {customerMessage}
             {
-                customers.map(
+                customers.slice(0,5).map(
                     (customerObj) => {
-                    return <h3 key={`customer--${customerObj.id}`}>{ customerObj.name }</h3>
+                    return <p key={`customer--${customerObj.id}`}>{ customerObj.name }</p>
                 })
             }
         </>
--- src/components/employees/EmployeeList.js
+++ src/components/employees/EmployeeList.js
@@ -1,20 +1,32 @@
 import React, { useEffect, useState } from 'react';
 
 export const EmployeeList = () => {
-    const [ employees, setEmployees ] = useState([])
+    const [ employees, setEmployees ] = useState([]);
+    const [ specialties, setSpecialties ] = useState("");
 
     useEffect(
         () => {
             fetch("http://localhost:8088/employees")
                 .then(res => res.json())
-                .then(setEmployees)
+                .then((data) => {
+                    setEmployees(data)
+                })
         },
         []
+    )
+
+    useEffect(
+        () => {
+            const specialty = employees.map(empObj => empObj.specialty)
+            setSpecialties(specialty.join(", "))
+        },
+        [employees]
     )
 
     return (
         <>
         <h2>Employees List</h2>
+            <div>Specialties: {specialties}</div>
             {
                 employees.map(
                     (empObj) => {
